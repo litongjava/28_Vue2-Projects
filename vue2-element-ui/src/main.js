0d7da52ab4873eb701c3d746dbc1b465034a017b
litongjava <litongjava@qq.com>
2023-04-16 20:13:19 +0800

    add vue-route

--- vue2-element-ui/src/main.js
+++ vue2-element-ui/src/main.js
@@ -6,10 +6,14 @@ Vue.config.productionTip = false
 
 //引入 element-ui
 import ElementUI from 'element-ui'
+
 //引入 element-ui 的 css 文件
 import 'element-ui/lib/theme-chalk/index.css';//声明使用 element-ui
+
+import router from './router'
 Vue.use(ElementUI);
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
